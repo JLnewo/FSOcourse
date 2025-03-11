2ac925a7c2e63b110300563c15d7c0d8f882661e
JLopez <lopezjorge719@gmail.com>
2025-03-10 20:16:59 -0500

    ej1-6

--- Part1/React1/part1/src/App.jsx
+++ Part1/React1/part1/src/App.jsx
@@ -1,73 +1,42 @@
-
-const Header = (props)=>{
-  return(
-    <div>
-      <p>Nombre del curso = {props.course} </p>
-    </div>
-  )
-}
-
-const Part =(props)=>{
-  const Dataparts = props.data
-  return(
-    <div>
-      <p>{Dataparts[props.id].name } {Dataparts[props.id].exercises }</p>  
-   </div>
-  )
-}
-
-const Content = (props)=>{
-     
-  return(
-    
-    <div>
-      <p>PARTES</p>
-      
-      <Part id= {0} data= {props.p1}/>
-      <Part id= {1} data= {props.p1}/>
-      <Part id= {2} data= {props.p1}/>
-    </div>
-  )
-}
-const Total = (props)=>{
-  
-  return(
-    <div>
-  <p> Total = {props.p1[0].exercises+props.p1[0].exercises+props.p1[0].exercises}</p>
-  </div>
-  )
-}
-
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
 
+    const handleGood =()=>{
 
+      const updateGood =good+1
+      setGood (updateGood)
+      
+    }
+    const handleNeutral =()=>{
+      const updateNeutral = neutral+1
+      setNeutral(updateNeutral)
+      
+    }
+    const handleBad =()=>{
+      const updateBad=bad+1
+      setBad(updateBad)
+      
+    }
   return (
     <div>
-      <Header course={course.name} />
-      <Content p1={course.parts} />
-      <Total  p1={course.parts} />
-    </div>
-
+      <text style={{fontSize:20,fontWeight:'bold'}}>give feedback</text>
+      <p></p>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
+      <p></p>
+      <text style={{fontSize:20,fontWeight:'bold'}}>statistics</text>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     
+    </div>
   )
 }
 
