8f0a9633f5398c8f0b0660062f3a6a438b1af7f2
JLopez <lopezjorge719@gmail.com>
2025-03-10 20:31:56 -0500

    ej1-7

--- Part1/React1/part1/src/App.jsx
+++ Part1/React1/part1/src/App.jsx
@@ -5,24 +5,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll]= useState(0)
 
 
     const handleGood =()=>{
 
-      const updateGood =good+1
-      setGood (updateGood)
-      
+      setGood (good+1)
+      setAll(all+1)
     }
     const handleNeutral =()=>{
-      const updateNeutral = neutral+1
-      setNeutral(updateNeutral)
-      
+      setNeutral(neutral+1)
+      setAll(all+1)
     }
     const handleBad =()=>{
       const updateBad=bad+1
-      setBad(updateBad)
-      
+      setBad(bad+1)
+      setAll(all+1)
     }
+
   return (
     <div>
       <text style={{fontSize:20,fontWeight:'bold'}}>give feedback</text>
@@ -35,6 +35,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {all>0 ? ((good+(bad*-1))/all):0}</p>
+      <p>positive {all >0 ? (good/all)*100 : 0} %</p>
     
     </div>
   )
